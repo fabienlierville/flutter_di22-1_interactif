c475656811b084ac92bd82f8d23d2da3293cd4b2
Fabien LIERVILLE <fabien.lierville27@gmail.com>
2024-01-04 09:10:59 +0100

    Textfield

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,8 +8,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String textValue = "";
+  bool obscure = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Widgets Interactifs"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              TextField(
+                obscureText: obscure,
+                onChanged: (String? value){
+                  setState(() {
+                    textValue = value!;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: "Entrez votre mail",
+                  hintText: "test@test.com",
+                  suffixIcon: IconButton(
+                    icon: Icon((obscure) ? Icons.visibility_off : Icons.visibility),
+                    onPressed: (){
+                      setState(() {
+                        obscure = !obscure;
+                      });
+                    },
+                  )
+                ),
+              ),
+              Text(textValue)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
